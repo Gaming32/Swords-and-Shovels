89cccfc6a401a46c066eb1bd0467aabdf2a11ee9
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-04-23 16:08:35 -0500

    3.2.1

--- Assets/3. Game Manager/Scripts/GameManager.cs
+++ Assets/3. Game Manager/Scripts/GameManager.cs
@@ -1,8 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    private string currentLevelName = "";
+
+    List<AsyncOperation> loadOperations;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        loadOperations = new List<AsyncOperation>();
+
+        LoadLevel("Main");
+    }
+
+    void OnLoadOperationComplete(AsyncOperation ao)
+    {
+        if (loadOperations.Contains(ao))
+        {
+            loadOperations.Remove(ao);
+        }
+
+        Debug.Log("Load Complete.");
+    }
+
+    void OnUnloadOperationComplete(AsyncOperation ao)
+    {
+        Debug.Log("Unload Complete.");
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        if (ao == null)
+        {
+            Debug.LogError("[Game Manager] Unable to load level " + levelName);
+            return;
+        }
+        ao.completed += OnLoadOperationComplete;
+        loadOperations.Add(ao);
+
+        currentLevelName = levelName;
+    }
+
+    public void UnloadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+        if (ao == null)
+        {
+            Debug.LogError("[Game Manager] Unable to unload level " + levelName);
+            return;
+        }
+        ao.completed += OnUnloadOperationComplete;
+    }
 }
